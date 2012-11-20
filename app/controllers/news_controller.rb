1abdc2e193d5fc57fb142a7f2bf36a36a943770c
David Angga Prasetya <carsmetic.oc@gmail.com>
2012-11-20 21:41:28 +0700

    SAT-56, hide inactive news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,13 +3,13 @@ class NewsController < ApplicationController
   def index
     set_page_title("News")
 
-    @news = News.where(:is_archive => false).page(params[:page])
+    @news = News.where(:is_archive => false, :is_active => true).page(params[:page])
   end
 
   def show
     @news = News.by_slug(params[:id])
 
-    if @news.nil?
+    if @news.nil? || !@news.is_active
       flash[:error] = "News not found"
       redirect_to news_index_path
     else
