0155ecd88a6dcf79452ebaab89320e1ac416a2a9
David Angga Prasetya <carsmetic.oc@gmail.com>
2013-01-25 00:02:00 +0700

    Fix percentage

--- app/helpers/manage_polls_helper.rb
+++ app/helpers/manage_polls_helper.rb
@@ -6,7 +6,7 @@ module ManagePollsHelper
       if contributors == 0
         string_result = number_to_percentage(0, :precision => 0)
       else
-        string_result = number_to_percentage(option.hit_counter.to_f / contributors.to_f, :precision => 0)
+        string_result = number_to_percentage((option.hit_counter.to_f / contributors.to_f)*100, :precision => 2)
       end
       string_result << " #{option.option_desc}" 
       array_result << string_result
