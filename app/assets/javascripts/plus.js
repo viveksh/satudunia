60e5ede49705a86bd1a61e898886f66dce0b6ecf
David Angga Prasetya <carsmetic.oc@gmail.com>
2013-01-29 01:20:05 +0700

    SAT-158, 159 New landing page

---
+++ app/assets/javascripts/plus.js
@@ -0,0 +1,6 @@
+//= require plus/jquery.js
+//= require plus/jquery-1.7.1.min.js
+//= require plus/script.js
+//= require plus/jquery.bxslider.js
+//= require plus/tms-0.4.1.js
+//= require plus/jquery.easing.1.3.js
