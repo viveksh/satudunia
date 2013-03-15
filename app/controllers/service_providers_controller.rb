56ab9d22d4dc8460a9161a87f24775e83e7380ca
David Angga Prasetya <carsmetic.oc@gmail.com>
2013-03-16 00:20:20 +0700

    logged in user services map

--- app/controllers/service_providers_controller.rb
+++ app/controllers/service_providers_controller.rb
@@ -1,5 +1,5 @@
 class ServiceProvidersController < ApplicationController
-  before_filter :login_required, :except => [:index, :show]
+  before_filter :login_required
   before_filter :check_permissions, :except => [:index, :show]
   layout "plus"
 
