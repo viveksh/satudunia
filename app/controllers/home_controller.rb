ffcd55b26c1b18e4587b7f6de1faccd9b77f9d0d
Vivek Sharma <vivek.s@cisinlabs.com>
2013-04-02 20:42:21 +0530

    [resolved issues ]

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
 
   def index
     @body_id = "page1"
-
+    #default limit stepper variable
+    @DefaultlimitStepper = current_group.question_default_length
+    @collectionAll = Question.all.limit(@DefaultlimitStepper)
+    @timeNow = Time.now
     # redirect back to admin
     if session["from_admin_login"] && !current_user.blank? && current_user.admin?
       redirect_to '/admin'
