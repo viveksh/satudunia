031fb82ff2d1374b461d2a3c73c130c281060277
David Angga Prasetya <carsmetic.oc@gmail.com>
2013-04-11 01:11:38 +0700

    SAT-310 Fix admin navigation

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -10,9 +10,5 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery_ujs
-//= require ui.core.js
-//= require ui.spinner.js
-//= require counterStepper.js
 //= require farbtastic.js
 
