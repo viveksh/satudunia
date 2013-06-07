5800694cb6de374b0fd64c88066bc695eedb9ab9
Vivek Sharma <vivek.s@cisinlabs.com>
2013-06-07 20:34:20 +0530

    [sprint-40 resolved issues SAT-559, SAT-566, SAT-463 , SAT-564, SAT-565, SAT-567]

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -18,7 +18,16 @@ class ServiceProvider
   field :email, :type => String
   field :telephone, :type => String
   field :country, :type => String
-  field :recommend, :type => String
+  # newely added
+  field :review_text, :type=>String, :default=>"It is a long established fact that a reader will be distracted by 
+  the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a 
+  more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look 
+  like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default 
+  model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions 
+  have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+  field :review_rating, :type=>Integer, :default => 0
+  field :recommend, :type => Integer, :default => 0
+  field :consultation, :type => String
   
   xapit do 
     text :name
