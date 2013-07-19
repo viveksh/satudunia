84f7fec0636032554b31482972b50c87f5386d21
Vivek Sharma <vivek.s@cisinlabs.com>
2013-07-19 20:15:43 +0530

    [sprint 44  resolved issue sat-679]

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,19 +4,21 @@ class ContactController < ApplicationController
   def index
     @contact = Contact.new()
   end
-  
+  # send contact us action
   def send_contact_us
+    #success case
     if @contact = Contact.create(params[:contact])
     	respond_to do |format|
     		format.html{redirect_to contact_path,notice:'Contact Submitted'}
     		format.json{render json: @contact }
     	end
+    #failure case
     else
     	respond_to do |format|
     		format.html{redirect_to contact_path,alert:'Something went wrong'}
     		format.json{render json: @contact }
     	end
-
     end
+    #conditiona statement ends here
   end
 end
