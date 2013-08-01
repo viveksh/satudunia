44a95b0b5de9315ed1f69b294441342908c01d77
madhusudhan <madhusudan.m@cisinlabs.com>
2013-08-01 22:49:20 +0530

    [sprint 46 resolved issues SAT-790, SAT-833, SAT-840]

--- app/helpers/experimental_helper.rb
+++ app/helpers/experimental_helper.rb
@@ -1,5 +1,5 @@
 module ExperimentalHelper
 	def check_valid
-	  (params[:controller] == "contact" && params[:action] == "index") || (params[:controller] == "users" && params[:action] == "new")
+	  (params[:controller] == "contact" && params[:action] == "index") || (params[:controller] == "users" && params[:action] == "new") || (params[:controller] == "devise/unlocks" && params[:action] == "new")
 	end
 end
