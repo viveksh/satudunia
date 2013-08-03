5899f464b2e7c1e6f7e55631667df401135e9fb9
dinshaw <dinshaw.r@cisinlabs.com>
2013-08-03 18:20:54 +0530

    SPRINT-46 SAT-850, SAT-855,SAT-861

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,7 +1,14 @@
 class SurveyController < ApplicationController
   def index
-    set_page_title("News")
-
-    @news = News.where(:is_archive => false, :is_active => true).page(params[:page])
+    set_page_title("Surveys")
+    @surveys= current_user.profile_tiers
+    @survey1= current_user.profile_support
+    @survey2 = current_user.profile_tier2
+    @survey3 = current_user.profile_tier3
+    @survey4 = current_user.profile_tier4
+    @survey5 = current_user.profile_tier5
+    @survey6 = current_user.profile_tier6
+    @survey7 = current_user.profile_tier7
+    #@news = News.where(:is_archive => false, :is_active => true).page(params[:page])
   end
 end
