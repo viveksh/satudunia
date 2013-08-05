2b1af311567f7a4b5508d15b014d2702e04d580c
madhusudhan <madhusudan.m@cisinlabs.com>
2013-08-05 23:08:37 +0530

    [sprint 47 resolved issues SAT-874, SAT-875, SAT-877, SAT-878, SAT-879, SAT-880, SAT-883, SAT-886, SAT-887, SAT-888, SAT-889]

--- app/helpers/experimental_helper.rb
+++ app/helpers/experimental_helper.rb
@@ -2,4 +2,9 @@ module ExperimentalHelper
 	def check_valid
 	  (params[:controller] == "contact" && params[:action] == "index") || (params[:controller] == "users" && params[:action] == "new") || (params[:controller] == "devise/unlocks" && params[:action] == "new")
 	end
+
+	def check_pre_loader
+		(params[:controller] == "experimental/experimental" && params[:action] == "index") 
+	end
+
 end
